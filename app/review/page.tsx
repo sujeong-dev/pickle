19dad361f5cda92e5c1b2e427620e2e825dbe98f
구수정 <9sujeong.dev@gmail.com>
2026-04-23 15:58:47 +0900

    fix: searchparam suspense로 감싸기

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { ReviewPage } from "@/pages/review";
 
 export default function Page() {
-  return <ReviewPage />;
+  return (
+    <Suspense>
+      <ReviewPage />
+    </Suspense>
+  );
 }
